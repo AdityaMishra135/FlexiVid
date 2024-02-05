b759fcdda62d7f9fa37e22f3a2f64402768bdcde
notrealmaurya <loop.maurya@gmail.com>
2024-02-05 20:35:59 +0530

    commit

--- app/src/main/java/com/maurya/flexivid/MainActivity.kt
+++ app/src/main/java/com/maurya/flexivid/MainActivity.kt
@@ -1,23 +1,44 @@
 package com.maurya.flexivid
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.maurya.flexivid.databinding.ActivityMainBinding
+import com.maurya.flexivid.fragments.FoldersFragment
+import com.maurya.flexivid.fragments.VideosFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        
+        navController.navigate(R.id.videosFragment)
 
+        binding.bottomNavMainActivity.setOnNavigationItemSelectedListener { item ->
+            val selectedFragment: Any = when (item.itemId) {
+                R.id.foldersBottomNav -> navController.navigate(R.id.foldersFragment)
+                R.id.videosBottomNav -> navController.navigate(R.id.videosFragment)
+                else -> navController.navigate(R.id.videosFragment)
+            }
+
+            true
+        }
 
 
     }
