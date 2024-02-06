b0445ad89ffb6e14f109c3b53fe6a8763ef6b497
notrealmaurya <loop.maurya@gmail.com>
2024-02-06 17:45:43 +0530

    commit

--- app/src/main/java/com/maurya/flexivid/activity/PlayerActivity.kt
+++ app/src/main/java/com/maurya/flexivid/activity/PlayerActivity.kt
@@ -11,11 +11,20 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.SimpleExoPlayer.Builder
 import com.maurya.flexivid.MainActivity
 import com.maurya.flexivid.R
+import com.maurya.flexivid.dataEntities.FolderDataClass
+import com.maurya.flexivid.dataEntities.VideoDataClass
 import com.maurya.flexivid.databinding.ActivityFolderBinding
 import com.maurya.flexivid.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
     private lateinit var activityPlayerBinding: ActivityPlayerBinding
+
+
+    companion object {
+//        lateinit var player: ExoPlayer
+        var playerList: ArrayList<VideoDataClass> = arrayListOf()
+        var position: Int = -1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +32,20 @@ class PlayerActivity : AppCompatActivity() {
         setContentView(activityPlayerBinding.root)
 
 
+        initializeLayout()
+
+    }
+
+    private fun initializeLayout() {
+
+
+
+
+
+        createPlayer()
+    }
+
+    private fun createPlayer() {
         val player = Builder(this).build()
         activityPlayerBinding.playerViewPlayerActivity.player = player
 
@@ -30,6 +53,7 @@ class PlayerActivity : AppCompatActivity() {
         player.setMediaItem(mediaItem)
         player.prepare()
         player.play()
-
     }
+
+
 }
