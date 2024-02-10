b881ee52b126a8eef4d4da14ad077802cb47903a
notrealmaurya <loop.maurya@gmail.com>
2024-02-10 11:44:06 +0530

    commit

--- app/src/main/java/com/maurya/flexivid/fragments/SettingsFragment.kt
+++ app/src/main/java/com/maurya/flexivid/fragments/SettingsFragment.kt
@@ -1,17 +1,26 @@
 package com.maurya.flexivid.fragments
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.maurya.flexivid.R
 import com.maurya.flexivid.databinding.FragmentSettingsBinding
 import com.maurya.flexivid.databinding.FragmentVideosBinding
+import com.maurya.flexivid.databinding.PopupAboutDialogBinding
+import com.maurya.flexivid.databinding.PopupMoreFeaturesBinding
 import com.maurya.flexivid.util.SharedPreferenceHelper
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -72,8 +81,49 @@ class SettingsFragment : Fragment() {
         }
 
         fragmentSettingsBinding.aboutLayout.setOnClickListener {
-            val customDialogFragment = AboutDialogFragment()
-            customDialogFragment.show(childFragmentManager, "CustomDialogFragment")
+            val popUpDialog = LayoutInflater.from(requireContext())
+                .inflate(R.layout.popup_about_dialog, fragmentSettingsBinding.root, false)
+            val bindingPopUp = PopupAboutDialogBinding.bind(popUpDialog)
+            val dialog =
+                MaterialAlertDialogBuilder(requireContext(), R.style.PopUpWindowStyle).setView(popUpDialog)
+                    .setOnCancelListener {
+
+                    }
+                    .create()
+
+            bindingPopUp.aboutDialogThankyouButton.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            val textView = bindingPopUp.spannableTextViewDialog
+            val spannableString = SpannableString("If you'd like to share your thoughts or provide Feedback , please feel free to do so. Your input is valuable, and I'd appreciate hearing from you.❤\uFE0F\"\n ")
+
+            val clickableSpan = object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    val websiteUrl =
+                        "https://forms.gle/4gC2XzHDCaio7hUh8"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(websiteUrl))
+                    startActivity(intent)
+                }
+            }
+
+            spannableString.setSpan(
+                clickableSpan,
+                48, 56,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            val blueColor = Color.BLUE
+            spannableString.setSpan(
+                ForegroundColorSpan(blueColor),
+                48, 56,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            textView.text = spannableString
+            textView.movementMethod = LinkMovementMethod.getInstance()
+
+            dialog.show()
+
         }
 
         fragmentSettingsBinding.feedbackLayout.setOnClickListener {
